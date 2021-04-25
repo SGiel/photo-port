78f64e046071e4c6c0dee43c02533f235ec93269
Stephanie Giel <stephaniegiel@gmail.com>
2021-04-24 19:57:29 -0700

    App converted to SPA

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,40 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
 
   function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid.');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage('');
+      }
+      // console.log('errorMessage', errorMessage);
+    }
     // We use the spread operator, ...formState, so we can retain the other key-value pairs in this 
     //object. Without the spread operator, the formState object would be overwritten to only contain 
     // the name: value key pair.
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
     // setFormState({...formState, name: e.target.value })
   }
   // console.log(formState);
-  
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log(formState);
@@ -25,16 +46,24 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+          {/* Used onBlur rather than onChange, because the onBlur attribute will fire the event once the 
+          user has changed focus from the input field, */}
+          <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+          <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
         </div>
+        {/* If errorMessage has a truthy value, the <div> block will render. */}
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">Submit</button>
       </form>
     </section>
